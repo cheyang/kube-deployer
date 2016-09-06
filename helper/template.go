c5cfae38a780b9d3a62c328e3793bdf216035cf6
cheyang <cheyang@163.com>
2016-09-06 22:16:15 +0800

    change code strcuture

--- helper/template.go
+++ helper/template.go
@@ -10,7 +10,7 @@ const RootDir string = "/etc/.kube_aliyun/"
 func RenderTemplateToFile(content string, file *os.File, data interface{}) error {
 	tmpl := template.New(file.Name())
 	tmpl.Delims("[[[", "]]]")
-	tmpl, err = tmpl.Parse(content)
+	tmpl, err := tmpl.Parse(content)
 	if err != nil {
 		return err
 	}
